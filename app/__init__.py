a1c21d76ac7d7610b9707f397b32994bad6814b4
adam <adam.lepkowski@hotmail.com>
2020-10-22 20:35:11 +0200

    Add app config

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from config import Config
 from app.main import bp as main_bp
 
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
+    app.config.from_object(config_class)
     app.register_blueprint(main_bp)
     return app
