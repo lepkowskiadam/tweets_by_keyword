c7b9ba4105f75a0df33b33c6cbd0dcc1d472e320
adam <adam.lepkowski@hotmail.com>
2020-10-22 21:10:49 +0200

    Register auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from config import Config
 from app.main import bp as main_bp
+from app.auth import bp as auth_bp
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp)
     return app
