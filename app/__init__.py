ba5fb11fe5d9d1ab1e8d310b59a81b17a57ffdf7
adam <adam.lepkowski@hotmail.com>
2020-10-25 19:39:51 +0100

    Add db and migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from app.main import bp as main_bp
 from app.auth import bp as auth_bp
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    db.init_app(app)
+    migrate.init_app(app, db)
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
     return app
+
+
+from app import models
