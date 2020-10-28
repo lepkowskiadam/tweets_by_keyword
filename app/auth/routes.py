9aceb66de00e00ce189e516e3dfd1d226d2ee78a
adam <adam.lepkowski@hotmail.com>
2020-10-28 17:38:28 +0100

    Add password validation/logout

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,12 +1,26 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 from app.auth import bp as auth_bp
 from app.auth.forms import LoginForm
 
 
 @auth_bp.route('/login', methods=["POST", "GET"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'{form.username.data} loggin in, remember_me={form.remember_me.data}')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('auth.login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('main.index'))
     return render_template('auth/login.html', form=form, title='Sign in')
+
+
+@auth_bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
