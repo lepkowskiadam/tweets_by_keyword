241dec8276cd9f88f378475094389d9735d4d07b
adam <adam.lepkowski@hotmail.com>
2020-11-07 17:23:14 +0100

    Add registration route

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -2,7 +2,8 @@ from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user
 from app.models import User
 from app.auth import bp as auth_bp
-from app.auth.forms import LoginForm
+from app.auth.forms import LoginForm, RegistrationForm
+from app import db
 
 
 @auth_bp.route('/login', methods=["POST", "GET"])
@@ -24,3 +25,20 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
+
+
+@auth_bp.route('/register', methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registered successfully')
+        return redirect(url_for('main.index'))
+    return render_template('auth/register.html', title='Register', form=form)
+
