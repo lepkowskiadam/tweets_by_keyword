b85dda65b79bbf4427a4d71191fbba02172b2c93
adam <adam.lepkowski@hotmail.com>
2020-11-08 22:01:28 +0100

    Add FollowUserForm

---
+++ app/main/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
+from app.models import User
+from flask_login import current_user
+
+
+class FollowUserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    submit = SubmitField('Add')
+
+    # TODO validation does not work correctly atm
+    def validate_follow(self, username):
+        followed = [user.username for user in current_user.followed.all()]
+        if username.data in followed:
+            raise ValidationError('You already follow this user')
+
