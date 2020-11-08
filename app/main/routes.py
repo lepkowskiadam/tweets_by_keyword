5245c34d8ae1cc142248bad25a825e5aafec3fc2
adam <adam.lepkowski@hotmail.com>
2020-11-08 09:13:03 +0100

    Add login requirement to view index

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,11 @@
 from flask import render_template
+from flask_login import login_required
 from app.main import bp
 
 
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
+@login_required
 def index():
     title = 'tweets by keyword'
     return render_template('index.html', title=title)
