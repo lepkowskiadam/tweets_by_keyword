48472328c36269822b23af173b4d722360acd936
adam <adam.lepkowski@hotmail.com>
2020-11-10 20:14:22 +0100

    Add init tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,17 +13,23 @@ def client():
     app = create_app(TestConfig)
     app.testing = True
 
-    client = app.test_client()
     with app.app_context():
         db.create_all()
         user = User(username='test_user', email='test@test.com')
         user.set_password('test_pw')
-        followed = Followed(username='test_followed_user', user_id=user.id)
+        followed = Followed(username='test_followed_user', follower=user)
         db.session.add(user)
         db.session.add(followed)
         db.session.commit()
-    yield client
+    return app
 
 
 def test_client(client):
-    assert client
+    with client.app_context():
+        u = User.query.all()
+        f = Followed.query.all()
+        assert u[0].username == 'test_user'
+        assert f[0].username == 'test_followed_user'
+        assert u[0].followed.one() == f[0]
+        assert len(u) == 1
+        assert len(f) == 1
