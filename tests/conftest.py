8780d358628d06eaf80d1b3ac39446524accd64a
adam <adam.lepkowski@hotmail.com>
2020-11-21 14:27:27 +0100

    Move fixtures to conftest

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+from app import create_app, db
+from app.models import User, Followed
+from config import Config
+import pytest
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    WTF_CSRF_ENABLED = False
+    TESTING = True
+
+
+@pytest.fixture
+def client():
+    app = create_app(TestConfig)
+    app.testing = True
+
+    with app.app_context():
+        db.create_all()
+        user = User(username='test_user', email='test@test.com')
+        user.set_password('test_pw')
+        followed = Followed(username='test_followed_user', follower=user)
+        db.session.add(user)
+        db.session.add(followed)
+        db.session.commit()
+    return app
+
+
+@pytest.fixture
+def user(client):
+    with client.app_context():
+        u = User.query.filter_by(username='test_user').first()
+    return u
