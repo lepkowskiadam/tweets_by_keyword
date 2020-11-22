f99f2b599fa9c4a629987c1b15487367c938a55b
adam <adam.lepkowski@hotmail.com>
2020-11-22 22:04:41 +0100

    Add username verification test

--- tests/test_twitter_handler.py
+++ tests/test_twitter_handler.py
@@ -12,3 +12,11 @@ def test_auth(handler):
     assert handler
     assert [] == handler.api.home_timeline()
 
+
+@pytest.mark.parametrize('username, expected', [
+    ('Twitter', True),
+    ('xxxxxxxxxxxxxxxx', False)  # twitter only allows 15 character long usernames so this will always return False
+])
+def test_verify_user_exists(handler, username, expected):
+    result = handler.verify_user_exists(username)
+    assert result == expected
