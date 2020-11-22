35803c4f6d4be84647f4a4580988c130d816ca50
adam <adam.lepkowski@hotmail.com>
2020-11-22 22:04:23 +0100

    Add username verification

--- twitter_handler.py
+++ twitter_handler.py
@@ -15,3 +15,10 @@ class TwitterHandler:
         self.auth = tweepy.OAuthHandler(self.consumer_key, self.secret_key)
         self.auth.set_access_token(self.access_token, self.access_secret_token)
         self.api = tweepy.API(self.auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+    def verify_user_exists(self, username):
+        try:
+            self.api.get_user(username)
+        except tweepy.error.TweepError:
+            return False
+        return True
