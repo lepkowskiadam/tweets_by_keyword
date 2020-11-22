1e2dbf78aa5c5baeca2395eed359292723d5b80e
adam <adam.lepkowski@hotmail.com>
2020-11-22 21:58:46 +0100

    Add TwitterHandler auth

---
+++ twitter_handler.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import tweepy
+import os
+
+load_dotenv()
+
+
+class TwitterHandler:
+    consumer_key = os.getenv('CONSUMER_KEY')
+    secret_key = os.getenv('SECRET_KEY')
+    access_token = os.getenv('ACCESS_TOKEN')
+    access_secret_token = os.getenv('ACCESS_SECRET_TOKEN')
+
+    def __init__(self):
+        self.auth = tweepy.OAuthHandler(self.consumer_key, self.secret_key)
+        self.auth.set_access_token(self.access_token, self.access_secret_token)
+        self.api = tweepy.API(self.auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
