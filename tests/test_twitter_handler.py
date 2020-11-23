1cd9df4cc2c6629d97a5ddbb1a567c20de407397
adam <adam.lepkowski@hotmail.com>
2020-11-23 21:11:58 +0100

    Add mock objects to api requests

--- tests/test_twitter_handler.py
+++ tests/test_twitter_handler.py
@@ -1,4 +1,6 @@
 from twitter_handler import TwitterHandler
+from unittest.mock import patch, Mock
+import tweepy
 import pytest
 
 
@@ -9,14 +11,18 @@ def handler():
 
 
 def test_auth(handler):
-    assert handler
-    assert [] == handler.api.home_timeline()
+    with patch.object(handler, 'api') as api_mock:
+        api_mock.home_timeline = Mock(return_value=[])
+        assert handler
+        assert [] == handler.api.home_timeline()
 
 
 @pytest.mark.parametrize('username, expected', [
     ('Twitter', True),
-    ('xxxxxxxxxxxxxxxx', False)  # twitter only allows 15 character long usernames so this will always return False
+    ('xxxxxxxxxxxxxxxx', False)  # twitter only allows 15 character long usernames so this should always return False
 ])
 def test_verify_user_exists(handler, username, expected):
-    result = handler.verify_user_exists(username)
-    assert result == expected
+    return_value = True if len(username) <= 15 else False
+    with patch.object(handler, 'verify_user_exists', return_value=return_value):
+        result = handler.verify_user_exists(username)
+        assert result == expected
