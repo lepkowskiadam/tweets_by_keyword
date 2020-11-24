069903ac9666a2ffb399797a69574df63233eac4
adam <adam.lepkowski@hotmail.com>
2020-11-24 20:58:59 +0100

    Add unfollow user form

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,10 +6,19 @@ from flask_login import current_user
 
 class FollowUserForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    submit = SubmitField('Add')
+    follow_submit = SubmitField('Follow')
 
     def validate_username(self, username):
         followed = [user.username for user in current_user.followed.all()]
         if username.data in followed:
             raise ValidationError('You already follow this user')
 
+
+class UnfollowUserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    unfollow_submit = SubmitField('Unfollow')
+
+    def validate_username(self, username):
+        followed = [user.username for user in current_user.followed.all()]
+        if username.data not in followed:
+            raise ValidationError('User not found in followed list')
