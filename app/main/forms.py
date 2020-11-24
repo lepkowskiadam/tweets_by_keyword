8ff9d0704ec0efb45960ecdb2bd9792c453873b5
adam <adam.lepkowski@hotmail.com>
2020-11-24 22:04:07 +0100

    Refactor field names

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,20 +5,20 @@ from flask_login import current_user
 
 
 class FollowUserForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username_follow = StringField('Follow', validators=[DataRequired()])
     follow_submit = SubmitField('Follow')
 
-    def validate_username(self, username):
+    def validate_username_follow(self, username):
         followed = [user.username for user in current_user.followed.all()]
         if username.data in followed:
             raise ValidationError('You already follow this user')
 
 
 class UnfollowUserForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username_unfollow = StringField('Unfollow', validators=[DataRequired()])
     unfollow_submit = SubmitField('Unfollow')
 
-    def validate_username(self, username):
+    def validate_username_unfollow(self, username):
         followed = [user.username for user in current_user.followed.all()]
         if username.data not in followed:
             raise ValidationError('User not found in followed list')
