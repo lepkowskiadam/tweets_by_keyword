6c80033b21af9e862c6dd872f528fab3372aca78
adam <adam.lepkowski@hotmail.com>
2020-12-01 20:35:57 +0100

    Refacor TwitterHandler
    Refacor class so that it uses classmethods rather than instance methods
    so that it won't have to be instantiated multiple times or bound to
    the app in config.

--- twitter_handler.py
+++ twitter_handler.py
@@ -10,23 +10,23 @@ class TwitterHandler:
     secret_key = os.getenv('SECRET_KEY')
     access_token = os.getenv('ACCESS_TOKEN')
     access_secret_token = os.getenv('ACCESS_SECRET_TOKEN')
+    auth = tweepy.OAuthHandler(consumer_key, secret_key)
+    auth.set_access_token(access_token, access_secret_token)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-    def __init__(self):
-        self.auth = tweepy.OAuthHandler(self.consumer_key, self.secret_key)
-        self.auth.set_access_token(self.access_token, self.access_secret_token)
-        self.api = tweepy.API(self.auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
-    def verify_user_exists(self, username):
+    @classmethod
+    def verify_user_exists(cls, username):
         try:
-            self.api.get_user(username)
+            cls.api.get_user(username)
         except tweepy.error.TweepError:
             return False
         return True
 
-    def get_tweets(self, username, keyword):
-        if self.verify_user_exists(username):
+    @classmethod
+    def get_tweets(cls, username, keyword):
+        if cls.verify_user_exists(username):
             tweets = []
-            for tweet in tweepy.Cursor(self.api.user_timeline, screen_name=username).items(20):
+            for tweet in tweepy.Cursor(cls.api.user_timeline, screen_name=username).items(20):
                 if keyword.lower() in tweet.text.lower():
                     tweets.append((tweet.text, tweet.id))
             return tweets
