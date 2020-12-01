6a6af514362bce7fb4f8aea97cc3c65e6320c10e
adam <adam.lepkowski@hotmail.com>
2020-12-01 20:34:03 +0100

    Refactor env variable name

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('APP_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
