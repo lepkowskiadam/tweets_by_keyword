fa79cad49b8476f2b781a0f470054c89824148ac
adam <adam.lepkowski@hotmail.com>
2020-12-15 19:53:27 +0100

    Add get tweets form

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, ValidationError
 from flask_login import current_user
+
 from twitter_handler import TwitterHandler
 
 
@@ -25,6 +26,11 @@ class UnfollowUserForm(FlaskForm):
         followed = [user.username for user in current_user.followed.all()]
         if username.data not in followed:
             raise ValidationError('User not found in followed list')
+
+
+class GetTweetsForm(FlaskForm):
+    keyword = StringField('keyword', validators=[DataRequired()])
+    keyword_submit = SubmitField('Get tweets')
 
 
 class BlankForm(FlaskForm):
